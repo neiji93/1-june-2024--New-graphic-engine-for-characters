fea460330f979b64a14bed912499218bfa2846b2
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-02 15:28:01 +0200

    Create Svbrdf.hlsl

---
+++ Svbrdf/Svbrdf.hlsl
@@ -0,0 +1,6 @@
+//there must be z relationship between
+//signals and ....
+//then turn hemisphere dampli'g to signal samoling
+//it there is a relationship with taylor seried too 
+
+//as this is taylor series, we need assumptions for models.
