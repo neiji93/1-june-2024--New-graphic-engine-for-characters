d1867676f95175dfaf10ae8c0b0a1bb9919675e8
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-04 11:34:33 +0200

    Update Svbrdf.hlsl

--- Svbrdf/Svbrdf.hlsl
+++ Svbrdf/Svbrdf.hlsl
@@ -4,3 +4,16 @@
 //it there is a relationship with taylor seried too 
 
 //as this is taylor series, we need assumptions for models.
+
+
+
+
+//assumptions on models would fail even
+//with steong signals datas. in order to fulffill this issue
+//we have to use orher motion capture repositories.
+//then it means to use shadow maps because of non supercomputer high rays counts.
+bool dhadowmapoverraystiles()
+
+
+//a more decure way will be to not using ais
+//for rayz. then we can use a lookup table and stacks.
